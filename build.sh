ef23e32527d1b2211a2b7d7f9c8a33374d1021fa
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-03-11 14:29:09 +0100

    no 1

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mvn -B package
+cp src/main/docker/Dockerfile target/
+docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
+docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
+docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+
+
+
